ba60838974ef6f0199a66d36909e42128a93baf3
Juan Manuel Vargas <juanmavargasr@gmail.com>
2024-05-21 10:23:43 -0500

    Implementación de filtro de restaurante por subdominio, se deja filtro quemado en el código para efectos de testing

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -4,7 +4,16 @@ const Restaurante = require("../models/restaurantesModel");
 
 const obtenerProductos = async (req, res) => {
   try {
-    const productosEncontrados = await Producto.find({})
+    const restaurante = await Restaurante.findOne({ nombre: "El Buen Sabor" }); //buscar la manera de pasar este restaurante como parámetro de la url
+
+    if (!restaurante) {
+      res.status(404).json({
+        mensaje: "restaurante no encontrado",
+      });
+    }
+    const productosEncontrados = await Producto.find({
+      restauranteId: restaurante._id,
+    })
       .populate("categoriaId", "nombre")
       .populate("restauranteId", "nombre");
     if (!productosEncontrados) {
@@ -12,7 +21,7 @@ const obtenerProductos = async (req, res) => {
         mensaje: "productos no entonctrados",
       });
     } else {
-      console.log(productosEncontrados);
+      //   console.log(productosEncontrados);
       res.status(200).json({
         mensaje: "Productos encontrados satisfactoriamente",
         Productos: productosEncontrados,
